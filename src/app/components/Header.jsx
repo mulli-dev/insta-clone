1218c928e0374709e35ab2975c4f212e4103c59e
Jacob Mulli <jacobmullie2@gmail.com>
2024-06-01 14:22:02 +0300

    add modal functionality using react modal

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,9 +5,15 @@ import React from "react";
 import Image from "next/image";
 import { signIn, useSession, signOut } from "next-auth/react";
 import { IoMdAddCircleOutline } from "react-icons/io";
+import Modal from "react-modal";
+import { useState } from "react";
+import { HiCamera } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function Header() {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
+
   console.log(session);
 
   return (
@@ -48,6 +54,7 @@ export default function Header() {
               className="text-2xl transition duration-300 cursor-pointer tranform hover:scale-125 hover:text-red-600"
               onClick={() => setIsOpen(true)}
             />
+
             <img
               src={session.user.image}
               alt={session.user.name}
@@ -64,6 +71,37 @@ export default function Header() {
           </button>
         )}
       </div>
+      {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+          onRequestClose={() => setIsOpen(false)}
+          ariaHideApp={false}
+        >
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            <HiCamera className="text-5xl text-gray-400 cursor-pointer" />
+          </div>
+          <input
+            type="text"
+            maxLength="150"
+            placeholder="Please enter you caption..."
+            className="w-full m-4 text-center border-none outline-none focus:ring-0"
+            onChange={(e) => setCaption(e.target.value)}
+          />
+          <button className="w-full p-2 text-white bg-red-600 rounded-lg shadow-md hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100">
+            Upload Post
+          </button>
+          <AiOutlineClose
+            className="absolute transition duration-300 cursor-pointer top-2 right-2 hover:text-red-600"
+            onClick={() => setIsOpen(false)}
+          />
+
+          <div>
+            <h1>Modal</h1>
+            <button onClick={() => setIsOpen(false)}>Close</button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 }
