6fda6722c6e4d63948520f19560da5068716fc4f
Jacob Mulli <jacobmullie2@gmail.com>
2024-06-03 19:03:34 +0300

    complete the post card of the feed section

--- src/app/components/Feed.jsx
+++ src/app/components/Feed.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 import MiniProfile from "./MiniProfile";
+import Posts from "./Posts";
 
 export default function Feed() {
   return (
     <main className="grid grid-cols-1 mx-auto md:grid-cols-3 md:max-w-6xl">
       {/* Posts (Left) */}
-      <section className="md:col-span-2"></section>
+      <section className="md:col-span-2">
+        <Posts />
+      </section>
 
       {/* MiniProfile (Right) */}
       <section className="hidden md:inline-grid md:col-span-1">
